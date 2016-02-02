a5f20bc9fcd5303edad5356cb28eb78e83d34c73
jtran064 <jtran064@ucr.edu>
2016-02-02 03:42:41 -0800

    comments along with grade

--- gradeSheet.py
+++ gradeSheet.py
@@ -9,6 +9,7 @@ from oauth2client.client import SignedJwtAssertionCredentials
 STATIC_COLUMNS = [ 'Last Name', 'First Name', 'Username', 'Student ID',
                     'Section', 'TOTAL', '']
 SPREADSHEET='CS100 Schedule & Grades'
+COMMENTS='Comments'
 
 # hack type exception thrown
 def get_first_json(str):
@@ -30,6 +31,9 @@ def set_up_argparse():
     parser.add_argument('-s', '--spreadsheet', metavar='S', type=str,
             default=SPREADSHEET,
             help='spreadsheet file name')
+    parser.add_argument('-w', '--worksheet', metavar='W', type=int,
+            default=1,
+            help='index of the worksheet starting at 0')
     return parser.parse_args()
 
 def login_with_key(creds):
@@ -44,7 +48,7 @@ def get_relevant_columns(wks):
     section_list = wks.row_values(1)
     column_dict = dict(zip(section_list, list(range(1, len(section_list)))))
     for col in STATIC_COLUMNS : # remove all unneeded columns
-        del column_dict[col]
+        column_dict.pop(col, None)
     print ("\nEditing Columns:")
     for i in column_dict:
         print (i)
@@ -62,7 +66,6 @@ def prompt_for_student(wks):
 
         if len(cell_list) > 1 :
             print ("There are more than one student with that name:")
-            print ( cell_list )
             for i in range(len(cell_list)):
                 print ("%d. %s" % (i, get_student_info(wks, cell_list[i].row)))
             selection = -1
@@ -79,11 +82,31 @@ def prompt_for_student(wks):
     print ("%s selected" % get_student_info(wks, selected_student_row))
     return selected_student_row
 
+def parse_grade_input(string):
+    input = string.split(" ", 1)
+    try:
+        input[0] = int(input[0])
+    except:
+        raise RuntimeError("Incorrect input")
+    if(len(input) == 1):
+        input.append('')
+    return input
+
+def add_to_grades(key, grade_dict, grade_input):
+    if key != COMMENTS:
+        grade_dict[key] = grade_input[0]
+    if COMMENTS in grade_dict:
+        if key != COMMENTS:
+            comment_string = "%s: %s\n" % (key, grade_input[1])
+        else:
+            comment_string = "%s: %s" % (key, grade_input[1])
+        grade_dict[COMMENTS] += comment_string
+
 def main(argv):
     args = set_up_argparse()
     gc = login_with_key(args.credential)
     print (args.spreadsheet)
-    wks = gc.open(args.spreadsheet).get_worksheet(3)
+    wks = gc.open(args.spreadsheet).get_worksheet(args.worksheet)
 
     print ("\nSelecting worksheet %s" % wks.title)
 
@@ -98,10 +121,20 @@ def main(argv):
             selected_student_rows.append(prompt_for_student(wks))
 
         # prompt for grade in an orderly mannner
-        grade_dict = {}
+        #  grade_dict = {}
+        grade_dict = dict.fromkeys(column_dict, '')
+
         for key in sorted(column_dict, key=column_dict.__getitem__):
-            grade_dict[key] = (input("\n%s: " % key))
-        print (grade_dict)
+            #  grade_dict[key] = (input("\n%s: " % key))
+            parsed_input = []
+            try:
+                parsed_input = parse_grade_input(input("\n%s: " % key))
+            except RuntimeError as ex:
+                print(ex, ': default to empty string')
+                print('Usage: (int) (comments)')
+                parsed_input = ['', '']
+            add_to_grades(key, grade_dict, parsed_input)
+
         for column in grade_dict:
             for student in selected_student_rows:
                 wks.update_cell(student, column_dict[column], grade_dict[column])
